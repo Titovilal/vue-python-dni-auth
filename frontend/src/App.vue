fe4d7151d0cc038e0d77d9183fce2dbc65172673
Titovilal <scastellsjuan@gmail.com>
2023-07-02 00:19:24 +0200

    Create login view

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Navbar from "./components/Navbar.vue";
+import Login from "./components/Login.vue";
 </script>
 
 <template>
   <Navbar msg="Vite + Vue" />
+  <Login />
 </template>
 
 <style scoped></style>
---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="row justify-content-center">
+      <div class="col-md-6">
+        <h1 class="text-center mb-4 mt-5">Login</h1>
+  
+        <form id="loginForm">
+          <div class="mb-3">
+            <label class="form-label">Email</label>
+            <input name="email" type="email" class="form-control" required />
+          </div>
+  
+          <div class="mb-3">
+            <label class="form-label">Password</label>
+            <input
+              name="password"
+              type="password"
+              class="form-control"
+              required
+            />
+          </div>
+  
+          <div class="d-flex justify-content-start">
+            <button type="submit" class="btn btn-primary">Login</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </template>
+  
+  <script setup></script>
+  
+  <style lang="scss" scoped></style>
+  
