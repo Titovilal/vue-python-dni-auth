9eb3df17b47da6dd5e1e5d1bd5f526b074ba13fa
Titovilal <scastellsjuan@gmail.com>
2023-07-02 04:27:46 +0200

    Create Register & Start

---
+++ frontend/src/components/Register.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="row justify-content-center">
+    <div class="col-md-6">
+      <h1 class="text-center mb-4 mt-5">Register</h1>
+
+      <form id="registerForm">
+        <div class="mb-3">
+          <label class="form-label">User</label>
+          <input name="user" type="text" class="form-control" required />
+        </div>
+        <div class="mb-3">
+          <label class="form-label">Email</label>
+          <input name="email" type="email" class="form-control" required />
+        </div>
+
+        <div class="mb-3">
+          <label class="form-label">Password</label>
+          <input
+            name="password"
+            type="password"
+            class="form-control"
+            required
+          />
+        </div>
+
+        <div class="mb-3">
+          <label class="form-label">Confirm Password</label>
+          <input
+            name="confirmPassword"
+            type="password"
+            class="form-control"
+            required
+          />
+        </div>
+
+        <div class="d-flex justify-content-start mt-4">
+          <button type="submit" class="btn btn-primary">Register</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
---
+++ frontend/src/components/Start.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    class="container d-flex justify-content-center align-items-center vh-100"
+  >
+    <div class="col-md-4">
+      <h1 class="text-center mb-4">Id Validator</h1>
+      <div class="d-grid gap-3">
+        <button class="btn btn-primary btn-lg" onclick="location.href='#'">
+          Register
+        </button>
+        <button class="btn btn-primary btn-lg" onclick="location.href='#'">
+          Login User
+        </button>
+        <button class="btn btn-primary btn-lg" onclick="location.href='#'">
+          Login Admin
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
