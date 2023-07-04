d4617ad268fbd7e5b9b82e7be1b57576016dc05f
Titovilal <scastellsjuan@gmail.com>
2023-07-04 23:35:09 +0200

    Implement sign up for logic

--- backend/views.py
+++ backend/views.py
@@ -30,22 +30,6 @@ def get_user_details(id):
     return jsonify(result)
 
 
-@views_bp.route('/admins', methods=['GET'])
-def get_admins():
-    admins = Admins.query.all()
-    admin_schema = AdminSchema(many=True)
-    result = admin_schema.dump(admins)
-    return jsonify(result)
-
-
-@views_bp.route('/admins/<int:id>', methods=['GET'])
-def get_admin(id):
-    admin = Admins.query.get(id)
-    admin_schema = AdminSchema()
-    result = admin_schema.dump(admin)
-    return jsonify(result)
-
-
 @views_bp.route('/users/<string:username>', methods=['GET'])
 def get_user_by_username(username):
     user = Users.query.filter_by(username=username).first()
@@ -80,7 +64,6 @@ def login():
     username = data['username']
     password = data['password']
 
-    # Consulta la base de datos para verificar si las credenciales son válidas
     user = Users.query.filter_by(username=username, password=password).first()
     if user:
         return jsonify({'success': True})
@@ -94,40 +77,90 @@ def loginAdmin():
     username = data['username']
     password = data['password']
 
-    # Consulta la base de datos para verificar si las credenciales son válidas
-    admin = Admins.query.filter_by(username=username, password=password).first()
+    admin = Admins.query.filter_by(
+        username=username, password=password).first()
     if admin:
         return jsonify({'success': True})
     else:
         return jsonify({'success': False, 'message': 'Invalid credentials'})
 
 
-@views_bp.route('/users/<int:id>', methods=['PUT'])
+@views_bp.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    username = data['username']
+    password = data['password']
+
+    new_detail = Details()
+    db.session.add(new_detail)
+    db.session.flush()
+
+    new_user = Users(username=username, password=password, id_details=new_detail.id)
+
+    try:
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify({'success': True})
+    except:
+        db.session.rollback()
+        return jsonify({'success': False, 'message': 'Error registering user'})
+
+
+# PUT Routes
+
+
+@views_bp.route('/update-user/<id>', methods=['PUT'])
 def update_user(id):
-    data = request.get_json()
     user = Users.query.get(id)
+
     if not user:
         return jsonify({'success': False, 'message': 'User not found'})
-    user.name = data.get('name', user.name)
-    user.surname = data.get('surname', user.surname)
-    user.dni = data.get('dni', user.dni)
-    db.session.commit()
-    user_schema = UserSchema()
-    result = user_schema.dump(user)
-    return jsonify(result)
 
-
-@views_bp.route('/users/<int:id>/details', methods=['PUT'])
-def update_user_details(id):
     data = request.get_json()
-    user = Users.query.get(id)
-    if not user:
-        return jsonify({'success': False, 'message': 'User not found'})
+    username = data.get('username')
+    password = data.get('password')
+    name = data.get('name')
+    surname = data.get('surname')
+    dni = data.get('dni')
+    valid = data.get('valid')
+    email = data.get('email')
+    address = data.get('address')
+    phone = data.get('phone')
+    birthdate = data.get('birthdate')
+    photo = data.get('photo')
+    dni_front = data.get('dni_front')
+    dni_back = data.get('dni_back')
+
+    if username:
+        user.username = username
+    if password:
+        user.password = password
+    if name:
+        user.name = name
+    if surname:
+        user.surname = surname
+    if dni:
+        user.dni = dni
+    if valid is not None:
+        user.valid = valid
+
     details = Details.query.get(user.id_details)
-    details.birthdate = data.get('birthdate', details.birthdate)
-    details.email = data.get('email', details.email)
-    details.address = data.get('address', details.address)
+    if details:
+        if email:
+            details.email = email
+        if address:
+            details.address = address
+        if phone:
+            details.phone = phone
+        if birthdate:
+            details.birthdate = birthdate
+        if photo:
+            details.photo = photo
+        if dni_front:
+            details.dni_front = dni_front
+        if dni_back:
+            details.dni_back = dni_back
+
     db.session.commit()
-    detail_schema = DetailSchema()
-    result = detail_schema.dump(details)
-    return jsonify(result)
+
+    return jsonify({'success': True})
--- frontend/src/components/Register.vue
+++ frontend/src/components/Register.vue
@@ -3,19 +3,32 @@
     <div class="col-md-6">
       <h1 class="text-center mb-4 mt-5">Register</h1>
 
-      <form id="registerForm">
+      <form id="registerForm" @submit.prevent="register">
         <div class="mb-3">
           <label class="form-label">User</label>
-          <input name="user" type="text" class="form-control" required />
+          <input
+            v-model="username"
+            name="username"
+            type="text"
+            class="form-control"
+            required
+          />
         </div>
         <div class="mb-3">
           <label class="form-label">Email</label>
-          <input name="email" type="email" class="form-control" required />
+          <input
+            v-model="email"
+            name="email"
+            type="email"
+            class="form-control"
+            required
+          />
         </div>
 
         <div class="mb-3">
           <label class="form-label">Password</label>
           <input
+            v-model="password"
             name="password"
             type="password"
             class="form-control"
@@ -26,6 +39,7 @@
         <div class="mb-3">
           <label class="form-label">Confirm Password</label>
           <input
+            v-model="confirmPassword"
             name="confirmPassword"
             type="password"
             class="form-control"
@@ -34,18 +48,55 @@
         </div>
 
         <div class="d-flex justify-content-between mt-4">
-          <router-link to="/" class="btn btn-secondary">
-            Go back</router-link
-          >
-          <router-link to="/login" class="btn btn-primary">
-            Register</router-link
-          >
+          <router-link to="/" class="btn btn-secondary"> Go back</router-link>
+          <button type="submit" class="btn btn-primary">Register</button>
         </div>
       </form>
     </div>
   </div>
 </template>
 
-<script setup></script>
+<script>
+export default {
+  data() {
+    return {
+      username: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    };
+  },
+  methods: {
+    register() {
+      if (this.password !== this.confirmPassword) {
+        alert("Passwords do not match");
+        return;
+      }
+      fetch("http://localhost:5000/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: this.username,
+          email: this.email,
+          password: this.password,
+        }),
+      })
+        .then((resp) => resp.json())
+        .then((data) => {
+          if (data.success) {
+            this.$router.push(`/user-details/${this.username}`);
+          } else {
+            alert(data.message);
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+};
+</script>
 
 <style lang="scss" scoped></style>
