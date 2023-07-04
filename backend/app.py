826a721f6670a95e95b37d7d67ac1c1aa50b432b
Titovilal <scastellsjuan@gmail.com>
2023-07-04 04:39:53 +0200

    Flask Cors to acces to data

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from config import *
 from models import *
-from views import *
 from views import views_bp
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 app.register_blueprint(views_bp)
 app.config.from_object('config')
