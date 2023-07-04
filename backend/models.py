c55542afed3378d8ae95e280464fd87bf5275410
Titovilal <scastellsjuan@gmail.com>
2023-07-04 04:41:24 +0200

    Rewrite due to DB updates

--- backend/models.py
+++ backend/models.py
@@ -5,57 +5,55 @@ from base64 import b64encode
 db = SQLAlchemy()
 ma = Marshmallow()
 
-class Users(db.Model):
-    username = db.Column(db.String(64), primary_key=True)
-    password = db.Column(db.String(64))
-    id_details = db.Column(db.Integer, db.ForeignKey('details.id'))
-    admin = db.Column(db.Integer)
 
-    def __init__(self, username, password, id_details, admin):
-        self.username = username
-        self.password = password
-        self.id_details = id_details
-        self.admin = admin
+class Admins(db.Model):
+    __tablename__ = 'admins'
+    id = db.Column(db.Integer, primary_key=True)
+    user = db.Column(db.Text, nullable=False)
+    username = db.Column(db.String(255), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    admin = db.Column(db.Boolean, nullable=False)
 
-class UserSchema(ma.Schema):
-    class Meta:
-        fields = ('username', 'password', 'id_details', 'admin')
-
-user_schema = UserSchema()
-users_schema = UserSchema(many=True)
 
 class Details(db.Model):
+    __tablename__ = 'details'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.Text)
-    surname = db.Column(db.Text)
-    email = db.Column(db.Text)
-    address = db.Column(db.Text)
-    phone = db.Column(db.Text)
-    birthdate = db.Column(db.Date)
-    photo = db.Column(db.LargeBinary)
-    dni_front = db.Column(db.LargeBinary)
-    dni_back = db.Column(db.LargeBinary)
-    is_validated = db.Column(db.Boolean)
+    email = db.Column(db.Text, nullable=False)
+    address = db.Column(db.Text, nullable=False)
+    phone = db.Column(db.Text, nullable=False)
+    birthdate = db.Column(db.Date, nullable=False)
+    photo = db.Column(db.LargeBinary, nullable=False)
+    dni_front = db.Column(db.LargeBinary, nullable=False)
+    dni_back = db.Column(db.LargeBinary, nullable=False)
 
-    def __init__(self, name, surname, email, address, phone, birthdate, photo, dni_front, dni_back, is_validated):
-        self.name = name
-        self.surname = surname
-        self.email = email
-        self.address = address
-        self.phone = phone
-        self.birthdate = birthdate
-        self.photo = photo
-        self.dni_front = dni_front
-        self.dni_back = dni_back
-        self.is_validated = is_validated
 
-from base64 import b64encode
+class Users(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    user = db.Column(db.Text, nullable=False)
+    name = db.Column(db.Text, nullable=False)
+    surname = db.Column(db.Text, nullable=False)
+    dni = db.Column(db.Text, nullable=False)
+    valid = db.Column(db.Boolean, nullable=False)
+    id_details = db.Column(db.Integer, db.ForeignKey(
+        'details.id'), nullable=False)
+
+
+class AdminSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'user', 'username', 'password',
+                  'admin')
+
+
+admin_schema = AdminSchema()
+admins_schema = AdminSchema(many=True)
+
 
 class DetailSchema(ma.Schema):
     class Meta:
-        fields = ('id', 'name', 'surname', 'email', 'address', 'phone', 'birthdate', 'photo', 'dni_front', 'dni_back', 'is_validated')
+        fields = ('id', 'email', 'address', 'phone',
+                  'birthdate', 'photo', 'dni_front', 'dni_back')
 
-    # Created due to problems with marshmallow schema
     def dump(self, obj, many=None, **kwargs):
         if obj.photo:
             obj.photo = b64encode(obj.photo).decode('utf-8')
@@ -68,3 +66,13 @@ class DetailSchema(ma.Schema):
 
 detail_schema = DetailSchema()
 details_schema = DetailSchema(many=True)
+
+
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'user', 'name', 'surname',
+                  'dni', 'valid', 'id_details')
+
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
