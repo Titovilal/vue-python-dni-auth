faa4542fe6dc668a13fb0fd3f8b52887d16e3e89
Titovilal <scastellsjuan@gmail.com>
2023-07-05 06:09:31 +0200

    Change database

--- backend/config.py
+++ backend/config.py
@@ -1,2 +1,2 @@
-SQLALCHEMY_DATABASE_URI = 'mysql://root:''@localhost/vue-python-dni-auth-db'
+SQLALCHEMY_DATABASE_URI = 'mysql://root:''@localhost/vue-python-dni-auth-db-test'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
